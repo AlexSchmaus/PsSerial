ff963257ae1213b5d1989c76a6264e7cac4663d1
Alex Schmaus <94810049+AlexSchmaus@users.noreply.github.com>
2022-12-12 11:09:56 -0500

    cleanup junk

--- test1.ps1
+++
@@ -1,8 +0,0 @@
-﻿$port = New-Object System.IO.Ports.SerialPort
-$port.PortName = "COM6"
-$port.BaudRate = "9600"
-$port.Parity = "None"
-$port.DataBits = 8
-$port.StopBits = 1
-$port.ReadTimeout = 9000 # 9 seconds
-$port.DtrEnable = "true"
