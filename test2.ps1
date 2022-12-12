c2d0525139f3a3ecdbfe7687bb3a833de980f8e3
Alex Schmaus <94810049+AlexSchmaus@users.noreply.github.com>
2022-12-12 11:09:39 -0500

    init

---
+++ test2.ps1
@@ -0,0 +1,10 @@
+﻿$port = New-Object System.IO.Ports.SerialPort
+$port.PortName = "COM4"
+$port.BaudRate = "9600"
+$port.Parity = "None"
+$port.DataBits = 8
+$port.StopBits = 1
+$port.ReadTimeout = 9000 # 9 seconds
+$port.DtrEnable = "true"
+
+$port
---
+++ test3.ps1
@@ -0,0 +1,22 @@
+﻿$port = New-Object System.IO.Ports.SerialPort
+$port.PortName = "COM4"
+$port.BaudRate = "9600"
+$port.Parity = "None"
+$port.DataBits = 8
+$port.StopBits = 1
+$port.ReadTimeout = 9000 # 9 seconds
+$port.DtrEnable = "true"
+
+$port.Open()
+    do {
+        $line = $port.ReadLine()
+        Write-Host $line # Do stuff here
+
+        $Temp = $line.Substring(9,7)
+
+        if ($Temp -lt 70) {Write-Host "I'm Cold"}
+        elseif ($line -gt 72) {Write-Host "I'm hot!"}
+        else {Write-Host "Temp is OK"}
+
+    }
+    while ($port.IsOpen)
